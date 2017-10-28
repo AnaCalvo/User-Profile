fdaec33b748adfab3473a956898006476a63d6ac
Ana Calvo <anacber@gmail.com>
2017-10-28 08:17:39 +0200

    Initial Commit

---
+++ UserProfileApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserProfileApp
+//
+//  Created by Ana Calvo on 28/10/17.
+//  Copyright © 2017 Ana Calvo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
